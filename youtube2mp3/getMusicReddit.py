3154e4c8d48d76b3c439d54867a9651b3299e958
Kevin Ngo <ktngo09@gmail.com>
2011-04-14 21:11:58 -0700

    added python script to pull music from reddit's /r/listentothis

---
+++ youtube2mp3/getMusicReddit.py
@@ -0,0 +1,42 @@
+import urllib
+import sys
+import re
+import subprocess
+
+""" Example formats for Reddit's URLs
+ http://www.reddit.com/r/listentothis/top/?t=day
+ http://www.reddit.com/r/listentothis/top/?t=week#page=2
+ Usage: python getMusicReddit.py [LINK] [week]
+"""
+
+def getHTML(url):
+    """Fetch HTML via HTTP request"""
+    fd = urllib.urlopen(url)
+    page = fd.read()
+    return page
+
+def getLinks(page):
+    """Search for YouTube links within HTML page"""
+    li = re.findall('"(http://www.youtube.com/watch\?v=.*?)"', page)
+    return li
+
+if __name__ == '__main__':
+    """Get YT links from site and output links to file"""
+
+    if len(sys.argv) == 1:
+        page = getHTML("http://www.reddit.com/r/listentothis")
+    elif str(sys.argv[1]) == "week":
+        page = getHTML("http://www.reddit.com/r/listentothis/top?t=week")
+    else:
+        page = getHTML(sys.argv[1])
+
+    li = getLinks(page)
+
+    print "\n" + str(len(li)) + " links found\n"
+    print str(li) + "\n"
+
+    fd = open("Music", "a")
+    for link in li:
+        fd.write(link + "\n");
+    fd.close()
+
